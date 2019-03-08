fa20d9387a30b2c608d9a4a4976d28cee1942c2e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-08 05:51:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -11,17 +12,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
-  end
+    @user = User.find_by(username: params[:username])
+    if @user 
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else 
+      erb :error 
+    end 
+  end 
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear 
+    redirect '/'
   end
-
-
 end
 
